a682a4d503821fdc2b1c5467896a864e7e3e9726
Amadea <53259552+amadea-system@users.noreply.github.com>
2021-01-29 11:51:49 -0500

    Change Port

--- PK_Gateway.py
+++ PK_Gateway.py
@@ -58,6 +58,6 @@ def get_cached_fronters():
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s")
-    app.run(host="0.0.0.0", debug=False)
+    app.run(host="0.0.0.0", port=5003, debug=False)
     # app.run(host="0.0.0.0", port=8080, debug=True)
 
